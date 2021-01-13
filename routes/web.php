7e799a125e1075a75a458fd651e01bc39ef70689
savirafatika <savira.fatika@students.amikom.ac.id>
2021-01-13 14:27:14 +0700

    routing view

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::view('/', 'welcome');
+// Route::view('contact', 'contact');
+Route::view('series/show', 'series.show');
+Route::view('series/premium/create', 'series.premium.create');
+
+Route::get('cetak', function () {
+    $postBody = "Lorem ipsum, or lipsum as it is sometimes known,
+    is dummy text used in laying out print, graphic or web designs.
+    The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.";
+    return view('cetak', ['body' => $postBody]);
 });
