220ea351f67fc57940db5b57efe9e829cf4b00e4
savirafatika <savira.fatika@students.amikom.ac.id>
2021-01-14 15:51:36 +0700

    eloquent (model)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        // if (!$post) {
+        //     abort(404);
+        // }
+
         return view('post.show', compact('post'));
     }
 }
